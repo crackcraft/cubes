93c7ca45402117e787a736c259d509103d11b94f
Egor Ivanov <ivanove@netcracker.com>
2014-09-13 16:43:31 +0400

    added tasks

--- src/main/java/net/crackcraft/cubes/Main.java
+++ src/main/java/net/crackcraft/cubes/Main.java
@@ -1,5 +1,7 @@
 package net.crackcraft.cubes;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -7,42 +9,94 @@ import java.util.List;
  */
 public class Main {
 
-    private final static String TASK0 =
-            "  o  o o o  o  \n" +
-            " ooo ooooo oooo\n" +
-            "ooooo ooo oooo \n" +
-            " ooo ooooo oooo\n" +
-            "  o  o o o  o  \n" +
-            " o o  o o  o o \n" +
-            "oooo ooooo oooo\n" +
-            " oooo ooo oooo \n" +
-            "oooo ooooo oooo\n" +
-            "oo o o o  oo oo\n";
-
-    private final static String TASK1 =
-            "  o  o o o  o  \n" +
-            " ooo ooooo oooo\n" +
-            "ooooo ooo oooo \n" +
-            " ooo ooooo oooo\n" +
-            "  o  o o o  o  \n" +
-
-            " o o  o o  o o \n" +
-            "oooo ooooo oooo\n" +
-            " oooo ooo oooo \n" +
-            "oooo ooooo oooo\n" +
-            "oo o o o  oo oo";
-
-    public static void main(String[] args) {
-        Cube cube = new Cube(TASK1);
-        List<Solution> sols = cube.solve(false);
+    private static void solve(String name, String task) throws IOException {
+        Cube cube = new Cube(task);
+        List<Solution> sols = cube.solve(true);
 
         for (Solution sol : sols) {
             assert test(sol.unfold());
         }
 
+        System.out.println(name + ": "+sols.size()+" solution(s)");
+
+        FileWriter out = new FileWriter(name);
         for (Solution sol : sols) {
-            System.out.println(sol);
+            out.append(sol.toString());
+            out.append("--8<-----------\n");
         }
+        out.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+//        solve("test",
+//            "  o  o o o  o  \n" +
+//            " ooo ooooo oooo\n" +
+//            "ooooo ooo oooo \n" +
+//            " ooo ooooo oooo\n" +
+//            "  o  o o o  o  \n" +
+//            " o o  o o  o o \n" +
+//            "oooo ooooo oooo\n" +
+//            " oooo ooo oooo \n" +
+//            "oooo ooooo oooo\n" +
+//            "oo o o o  oo oo"
+//        );
+
+        solve("task1",
+            "  o  o o o  o  \n" +
+            " ooo ooooo oooo\n" +
+            "ooooo ooo oooo \n" +
+            " ooo ooooo oooo\n" +
+            "  o  o o o  o  \n" +
+
+            " o o  o o  o o \n" +
+            "oooo ooooo oooo\n" +
+            " oooo ooo oooo \n" +
+            "oooo ooooo oooo\n" +
+            "oo o o o  oo oo"
+        );
+
+        solve("task2",
+            "   oo o o  oo o\n" +
+            " ooo oooo ooooo\n" +
+            "ooooo oooo ooo \n" +
+            " ooo oooo ooooo\n" +
+            " o oo o   o  oo\n" +
+
+            "  o    oo  oo  \n" +
+            "oooo ooooo ooo \n" +
+            " oooo ooo ooooo\n" +
+            "oooo ooooo ooo \n" +
+            "  o  o o  oo oo"
+        );
+
+        solve("task3",
+            "oo o    oo o   \n" +
+            "oooo oooo oooo \n" +
+            "oooo ooooo oooo\n" +
+            " oooo ooo oooo \n" +
+            "  o   o o   o  \n" +
+
+            "oo oo  o o o oo\n" +
+            " oooo oooo ooo \n" +
+            "oooo ooooo oooo\n" +
+            " ooo oooo oooo \n" +
+            " o o o oo oo o "
+        );
+
+        solve("task4",
+            "  o    o o  o o\n" +
+            "oooo ooooo oooo\n" +
+            " oooo ooo oooo \n" +
+            "oooo oooo ooooo\n" +
+            " o o  o o o o  \n" +
+
+            "o o o  o   o o \n" +
+            "ooooo oooo ooo \n" +
+            " ooo oooo ooooo\n" +
+            "ooooo oooo ooo \n" +
+            "o o  oo o  o oo"
+        );
 
     }
 
